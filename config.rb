bda57be2f6c9b0a739469d919124e1b1bc487150
Chang Liu <shampliu@gmail.com>
2015-04-09 01:23:00 -0700

    sass + scrollspy

---
+++ config.rb
@@ -0,0 +1,12 @@
+http_path = "/" 
+css_dir = "css" 
+sass_dir = "scss" 
+images_dir = "img"
+images_path = "/Users/changliu/Desktop/dance-marathon-2015/img" 
+javascripts_dir = "js" 
+output_style = :expanded 
+relative_assets = true 
+line_comments = true
+# generated_images_path = "/images"
+# http_images_dir = "img"
+
