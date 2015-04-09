9feeec453e6b9a2db37cf977d3bf0950bba69b75
Chang Liu <shampliu@gmail.com>
2015-04-09 14:52:30 -0700

    scrollspy with JSON working

--- js/main.js
+++ js/main.js
@@ -15,28 +15,41 @@ $(document).ready(function() {
 	$(function showstones() {	
 	$.getJSON( "https://spreadsheets.google.com/feeds/list/1rQHDYJIHHKijCQPpxjUaO1r0oZn4fLEryNmNsnfX2Gg/od6/public/values?alt=json",
 		function (data) {	
+			var count = 1; 
 			$.each(data.feed.entry.reverse(), function(i,entry) {	
 			if (entry.gsx$type.$t == "post")
-		    {
-				var append = '<li class="accordion-navigation stone s'+(i%3+1)+'">';
+	    {
+				// var append = '<li class="accordion-navigation stone s'+(i%3+1)+'">';
+				// append += '<div class="stone-title" id="t'+i+'"> </div>';
+				// append += '<div id="panel'+i+'a" class="content stone-desc" id="desc'+i+'"></div>';
+				// append += '</li>';
+				// $('ul#stonewall').append(append);
+
+				var append = '<section id="anchor' + count + '">';
 				append += '<div class="stone-title" id="t'+i+'"> </div>';
 				append += '<div id="panel'+i+'a" class="content stone-desc" id="desc'+i+'"></div>';
-				append += '</li>';
-				$('ul#stonewall').append(append);
+				append += '</section>';
+				$('div#content').append(append); 
+				count++; 
+
 				var title = '<b>' + entry.gsx$title.$t + ':</b> ';
 				var desc = entry.gsx$content.$t;
 				$('#t'+i+'').append(title);
 				$('#panel'+i+'a').append(desc);
+
 			}
 
 			if (entry.gsx$type.$t == "video")
-		    {
-				var append = '<li class="accordion-navigation stone s'+(i%3+1)+'">';
+	    {
+	    	var append = '<section id="anchor' + count + '">';
+				// var append = '<li class="accordion-navigation stone s'+(i%3+1)+'">';
 				append += '<div class="stone-title" id="t'+i+'"> </div>';
 				append += '<div id="panel'+i+'a" class="content stone-desc" id="desc'+i+'"></div>';
 				append += '<div id="video"></div>';
-				append += '</li>';
-				$('ul#stonewall').append(append);
+				append += '</section>';
+				$('div#content').append(append); 
+				count++; 
+				// $('ul#stonewall').append(append);
 				var title = '<b>' + entry.gsx$title.$t + ':</b> ';
 				var desc = entry.gsx$content.$t;
 				var link = '<iframe width="560" height="315" src="https://www.youtube.com/embed/' + entry.gsx$link.$t + '" frameborder="0" allowfullscreen></iframe>'
@@ -48,43 +61,43 @@ $(document).ready(function() {
 
 			if (entry.gsx$type.$t == "image")
 	    {
-			var append = '<li class="accordion-navigation stone s'+(i%3+1)+'">';
-			append += '<div class="stone-title" id="t'+i+'"> </div>';
-			append += '<div id="panel'+i+'a" class="content stone-desc" id="desc'+i+'"></div>';
-			append += '<div id="pic"></div>';
-			append += '</li>';
-			$('ul#stonewall').append(append);
-			var title = '<b>' + entry.gsx$title.$t + ':</b> ';
-			var desc = entry.gsx$content.$t;
-			var link = ' <img src="'+entry.gsx$link.$t+'">'
-			$('#t'+i+'').append(title);
-			$('#panel'+i+'a').append(desc);
+				var append = '<li class="accordion-navigation stone s'+(i%3+1)+'">';
+				append += '<div class="stone-title" id="t'+i+'"> </div>';
+				append += '<div id="panel'+i+'a" class="content stone-desc" id="desc'+i+'"></div>';
+				append += '<div id="pic"></div>';
+				append += '</li>';
+				$('ul#stonewall').append(append);
+				var title = '<b>' + entry.gsx$title.$t + ':</b> ';
+				var desc = entry.gsx$content.$t;
+				var link = ' <img src="'+entry.gsx$link.$t+'">'
+				$('#t'+i+'').append(title);
+				$('#panel'+i+'a').append(desc);
 
-			$('#video').append(link);
-		}
+				$('#video').append(link);
+			}
 
-		if (entry.gsx$type.$t == "image")
-	    {
-			var append = '<li class="accordion-navigation stone s'+(i%3+1)+'">';
-			append += '<div class="stone-title" id="t'+i+'"> </div>';
-			append += '<div id="panel'+i+'a" class="content stone-desc" id="desc'+i+'"></div>';
-			append += '<div id="pic"></div>';
-			append += '</li>';
-			$('ul#stonewall').append(append);
-			var title = '<b>' + entry.gsx$title.$t + ':</b> ';
-			var desc = entry.gsx$content.$t;
-			var link = ' <img src="'+entry.gsx$link.$t+'">'
-			$('#t'+i+'').append(title);
-			$('#panel'+i+'a').append(desc);
+			if (entry.gsx$type.$t == "image")
+		    {
+					var append = '<li class="accordion-navigation stone s'+(i%3+1)+'">';
+					append += '<div class="stone-title" id="t'+i+'"> </div>';
+					append += '<div id="panel'+i+'a" class="content stone-desc" id="desc'+i+'"></div>';
+					append += '<div id="pic"></div>';
+					append += '</li>';
+					$('ul#stonewall').append(append);
+					var title = '<b>' + entry.gsx$title.$t + ':</b> ';
+					var desc = entry.gsx$content.$t;
+					var link = ' <img src="'+entry.gsx$link.$t+'">'
+					$('#t'+i+'').append(title);
+					$('#panel'+i+'a').append(desc);
 
-			$('#pic').append(link);
-		}
+					$('#pic').append(link);
+				}
 
-				});
 			});
+		});
 
 	  
-		});
+	});
 
   
 
