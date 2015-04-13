be906306ed631eb2f007015ddb0aa7069f20a4b9
vivianzhang61 <vivian-keke@hotmail.com>
2015-04-12 22:59:27 -0700

    Yayy automatically updated links! I THINK IT'S WORKING.

--- js/main.js
+++ js/main.js
@@ -5,6 +5,11 @@ $('#nav').affix({
     }
 });
 
+//number of elements in timeArray
+var cutoff,j,k=0;
+//time between 4/18 23:59 and 4/18 9:00
+var timeArray = ['4/19/2015 23:00:00','4/18/2015 23:00:00','4/18/2015 21:00:00','4/18/2015 19:00:00','4/18/2015 17:00:00','4/18/2015 15:00:00','4/18/2015 13:00:00','4/18/2015 11:00:00','4/18/2015 09:00:00'];
+var timeName = ["11 PM","9 PM","7 PM","5 PM","3 PM","1 PM", "11 AM","9 AM","7 AM"];
 
 $(document).ready(function() {	
 	var position = ["top","center","bottom"];
@@ -13,8 +18,33 @@ $(document).ready(function() {
 	$.getJSON( "https://spreadsheets.google.com/feeds/list/1rQHDYJIHHKijCQPpxjUaO1r0oZn4fLEryNmNsnfX2Gg/od6/public/values?alt=json",
 		function (data) {	
 			$.each(data.feed.entry.reverse(), function(i,entry) {	
+				var time = new Date (entry.gsx$datetime.$t);
+				if(i==0)
+				{
+					//find the latest time to start out with
+					for(j=1;j<timeArray.length;j++)
+					{
+						if(time <= new Date(timeArray[j-1]) && time >= new Date(timeArray[j]))
+						{
+							k=j-1;
+							var newLink = ' <li><a href="#anchor'+i+'">'+timeName[k]+'</a></li>'
+							$('ul.nav').append(newLink); 
+							cutoff=new Date(timeArray[j]);
+							k++;j++;
+							break;
+						}
+					}
+				}
+				else if (time <= cutoff)
+				{	
+					var newLink = ' <li><a href="#anchor'+i+'">'+timeName[k]+'</a></li>'
+					$('ul.nav').append(newLink); 
+					cutoff=new Date(timeArray[j]);
+					j++;k++;
+				}			
+
 			if (entry.gsx$type.$t == "post")
-	    {
+	   		{	
 				var append = '<section id="anchor' + i+ '">';
 				append += '<div class="post-title" id="t'+i+'"> </div>';
 				append += '<div id="panel'+i+'a" class="content stone-desc" id="desc'+i+'"></div>';
@@ -22,41 +52,48 @@ $(document).ready(function() {
 				$('div#content').append(append); 
 				var title = '<b>' + entry.gsx$title.$t + ':</b> ';
 				var desc = entry.gsx$content.$t;
+				var timeDate = entry.gsx$datetime.$t;
 				$('#t'+i+'').append(title);
+				$('#panel'+i+'a').append(timeDate+'<br>');
 				$('#panel'+i+'a').append(desc);
+				
 			}
 
-			if (entry.gsx$type.$t == "video")
-	    {
+			else if (entry.gsx$type.$t == "video")
+	    	{
 	    	var append = '<section id="anchor' + i+ '">';
 				append += '<div class="post-title" id="t'+i+'"> </div>';
 				append += '<div id="panel'+i+'a" class="content stone-desc" id="desc'+i+'"></div>';
-				append += '<div id="video"></div>';
+				append += '<div id="video'+i+'"></div>';
 				append += '</section>';
 				$('div#content').append(append); 
 				var title = '<b>' + entry.gsx$title.$t + ':</b> ';
 				var desc = entry.gsx$content.$t;
+				var timeDate = entry.gsx$datetime.$t;
 				var link = '<iframe width="560" height="315" src="https://www.youtube.com/embed/' + entry.gsx$link.$t + '" frameborder="0" allowfullscreen></iframe>'
 				$('#t'+i+'').append(title);
+				$('#panel'+i+'a').append(timeDate+'<br>');
 				$('#panel'+i+'a').append(desc);
-				$('#video').append(link);
+				$('#video'+i+'').append(link);
 			}
 
-			if (entry.gsx$type.$t == "image")
+			else if (entry.gsx$type.$t == "image")
 		    {
-					append += '<div class="post-title" id="t'+i+'"> </div>';
+		    	var append = '<section id="anchor' + i+ '">';
+				    append += '<div class="post-title" id="t'+i+'"> </div>';
 					append += '<div id="panel'+i+'a" class="content stone-desc" id="desc'+i+'"></div>';
-					append += '<div id="pic"></div>';
+					append += '<div id="pic'+i+'"></div>';
 					append += '</li>';
-					console.log(append);
 					$('div#content').append(append);
 					var title = '<b>' + entry.gsx$title.$t + ':</b> ';
 					var desc = entry.gsx$content.$t;
 					var link = ' <img src="'+entry.gsx$link.$t+'">'
+					var timeDate = entry.gsx$datetime.$t;
 					$('#t'+i+'').append(title);
+					$('#panel'+i+'a').append(timeDate+'<br>');
 					$('#panel'+i+'a').append(desc);
-					$('#pic').append(link);
-				}
+					$('#pic'+i+'').append(link);
+			}
 
 			});
 		});
