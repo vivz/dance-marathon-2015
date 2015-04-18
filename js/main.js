d4853687f8f19ef3ae9a3e98ce71ed8018a4e785
vivianzhang61 <vivian-keke@hotmail.com>
2015-04-18 12:11:02 -0700

    transition

--- js/main.js
+++ js/main.js
@@ -8,22 +8,22 @@ $('#nav').affix({
 });
 
 
-	function play(i) {
-	 var name ='#ytvideo'+i+'';
-  	 $(name)[0].src += "&autoplay=1";
-  	  ev.preventDefault();
-	}
+
+var tag = document.createElement('script');
+
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
 
 // var tOffset = $(window).width() - $('section').width(); 
 var tOffset = Math.floor(($(window).width() / 12) * 2.17); 
-console.log(tOffset);
+
 
 var w = $(window).width(); 
 var h = $(window).height(); 
 
-var transtionArray=[];
 //number of elements in timeArray
-var cutoff,j,k=0;
+var cutoff,j,k=0,p=0;
 //time between 4/18 23:59 and 4/18 9:00
 var timeArray = ['4/25/2015 23:00:00','4/19/2015 11:00:00','4/19/2015 09:00:00','4/19/2015 07:00:00','4/19/2015 05:00:00','4/19/2015 03:00:00','4/19/2015 01:00:00','4/18/2015 23:00:00','4/18/2015 21:00:00','4/18/2015 19:00:00','4/18/2015 17:00:00','4/18/2015 15:00:00','4/18/2015 13:00:00','4/18/2015 11:00:00','4/18/2015 09:00:00'];
 // var timeName = ["11 PM","9 PM","7 PM","5 PM","3 PM","1 PM", "11 AM","9 AM","7 AM"];
@@ -32,7 +32,7 @@ var timeName = ["26", "24", "22", "20","18","16","14","12","10","8","6","4","2",
 $(document).ready(function() {
 	//source file is https://docs.google.com/a/media.ucla.edu/spreadsheets/d/1rQHDYJIHHKijCQPpxjUaO1r0oZn4fLEryNmNsnfX2Gg/edit?pli=1#gid=0
 	$(function() {	
-		$.getJSON( "https://spreadsheets.google.com/feeds/list/1gJQIn0TvEJ0c-R7Csntfwmu3qwTzqAYJEImdZ720jeQ/od6/public/values?alt=json", function (data) {	
+		$.getJSON( "https://spreadsheets.google.com/feeds/list/1rQHDYJIHHKijCQPpxjUaO1r0oZn4fLEryNmNsnfX2Gg/od6/public/values?alt=json", function (data) {	
 			$.each(data.feed.entry.reverse(), function(i,entry) {	
 				var time = new Date (entry.gsx$datetime.$t);
 
@@ -69,13 +69,12 @@ $(document).ready(function() {
 					// $('div#content').append(append); 
 					var append = '<section class="transition">'
 					+'<div id="container" style="height: 100%; overflow:hidden; ">'
-       			+'<iframe id="player'+'" style="width:100%; height:100%;" '
+       			+'<iframe id="ytvideo'+i+'" style="width:100%; height:100%;" '
        			+'src="https://www.youtube.com/embed/'+entry.gsx$link.$t
-       			+'?controls=0&amp;loop=1&amp;showinfo=0&amp;modestbranding=1&amp;disablekb=1&amp;enablejsapi=1"'
+       			+'?autoplay=1&amp;controls=0&amp;loop=1&amp;showinfo=0&amp;modestbranding=1&amp;disablekb=1&amp;enablejsapi=1"'
            	+'frameborder="0" allowfullscreen></iframe></div></section>';
 					$('div#content').append(append); 
-					transtionArray.push(i);
-
+				//	console.log(i);
 				} 		
        		else if (entry.gsx$type.$t == "post") {	
 					var append = '<section id="anchor' + i+ '">';
@@ -187,17 +186,19 @@ $(document).ready(function() {
 			});
 
 
+
+
 		});
 
 
+	
 	  
 	});
-	
+
 	
 });
 
 setTimeout(function() {
-          
   $('.info').each(function() {
 
 	  if($(this).innerHeight() > 250){
@@ -211,106 +212,6 @@ setTimeout(function() {
 
 
 	}) 
-
-	
-    
 }, 800);
 
-var videos = document.getElementsByTagName("iframe"), fraction = 0.8;
 
-function checkScroll() {
-
-  for(var i = 0; i < videos.length; i++) {
-    var video = videos[i];
-
-    var x = 0,
-        y = 0,
-        w = video.width,
-        h = video.height,
-        r, //right
-        b, //bottom 
-        visibleX, visibleY, visible,
-        parent;
-
-    
-    parent = video;
-    while (parent && parent !== document.body) {
-      x += parent.offsetLeft;
-      y += parent.offsetTop;
-      parent = parent.offsetParent;
-    }
-
-    r = x + parseInt(w);
-    b = y + parseInt(h);
-   
-
-    visibleX = Math.max(0, Math.min(w, window.pageXOffset + window.innerWidth - x, r - window.pageXOffset));
-    visibleY = Math.max(0, Math.min(h, window.pageYOffset + window.innerHeight - y, b - window.pageYOffset));
-    
-
-    visible = visibleX * visibleY / (w * h);
-
-
-    if (visible > fraction) {
-      playVideo();
-    } else {
-      pauseVideo();
-
-    }
-  }
-
-};
-
-
-var tag = document.createElement('script');
-
-tag.src = "https://www.youtube.com/iframe_api";
-var firstScriptTag = document.getElementsByTagName('script')[0];
-firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
-
-// 3. This function creates an <iframe> (and YouTube player)
-//    after the API code downloads.
-var player;
-
-function onYouTubeIframeAPIReady() {
-  player = new YT.Player('player', {
-    events: {
-      'onReady': onPlayerReady,
-      'onStateChange': onPlayerStateChange
-    }
-  });
-};
-
-// 4. The API will call this function when the video player is ready.
-function onPlayerReady(event) {
-    window.addEventListener('scroll', checkScroll, false);
-    window.addEventListener('resize', checkScroll, false);
-
-    //check at least once so you don't have to wait for scrolling for the    video to start
-    window.addEventListener('load', checkScroll, false);
-};
-
-
-function onPlayerStateChange(event) {
-    if (event.data == YT.PlayerState.PLAYING) {
-      //console.log("event played");
-    } else {
-      //console.log("event paused");
-    }
-};
-
-function stopVideo() {
-    player.stopVideo();
-};
-
-function playVideo() {
-  player.playVideo();
-};
-
-function pauseVideo() {
-  player.pauseVideo();
-};
-
-
-
-	
