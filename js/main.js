aeabf2acb88f189f2b720c459cfd716f90bcee07
Simon Zou <simon.zou@gmail.com>
2015-04-18 11:56:14 -0700

    continue reading changed to expand

--- js/main.js
+++ js/main.js
@@ -205,7 +205,7 @@ setTimeout(function() {
 
 	  if($(this).innerHeight() > 250){
 	      $(this).readmore({
-	        moreLink: '<a style="color: black; font-weight: 700;" href="#">Continue Reading</a>',
+	        moreLink: '<a style="color: black; font-weight: 700;" href="#">Expand</a>',
 	  			lessLink: '<a style="color: black; font-weight: 700;" href="#">Close</a>',
 	        maxHeight: 228,
 	        speed: 200
